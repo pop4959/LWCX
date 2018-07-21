ad11a34a40643c5e2d1c24ffb09a6d492d790ca7
Me_Goes_RAWR <sammylover26@gmail.com>
2018-07-21 16:47:36 -0400

    Create LWCProtocoLib.java

---
+++ src/main/java/com/griefcraft/listeners/LWCProtocoLib.java
@@ -0,0 +1,42 @@
+package com.griefcraft.listeners;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.griefcraft.lwc.LWC;
+import com.griefcraft.lwc.LWCPlugin;
+
+public class LWCProtocoLib implements Listener {
+
+	private static LWCPlugin plugin;
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerInteract(PlayerInteractEvent event) {
+		LWC lwc = plugin.getLWC();
+		Player player = event.getPlayer();
+		Block block = event.getClickedBlock();
+		try {
+			if (lwc.findProtection(block) != null && !lwc.canAccessProtection(player, block)) {
+				ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(lwc.getPlugin(),
+						ListenerPriority.MONITOR, PacketType.Play.Server.OPEN_WINDOW) {
+					@Override
+					public void onPacketSending(PacketEvent e) {
+						e.setReadOnly(false);
+						e.setCancelled(true);
+						lwc.sendLocale(player, "protection.interact.error.notowner", "block", LWC.materialToString(event.getClickedBlock()));
+						return;
+					}
+				});
+			}
+		} catch (Exception e) {
+		}
+	}
+}
