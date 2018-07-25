888752f53e2763de680abc4a61d492082969e4d3
Me_Goes_RAWR <sammylover26@gmail.com>
2018-07-25 16:01:44 -0400

    Update LWCProtocoLib.java

--- src/main/java/com/griefcraft/listeners/LWCProtocoLib.java
+++ src/main/java/com/griefcraft/listeners/LWCProtocoLib.java
@@ -17,24 +17,35 @@ import com.griefcraft.lwc.LWCPlugin;
 public class LWCProtocoLib implements Listener {
 
 	private static LWCPlugin plugin;
+	
 	public LWCProtocoLib(LWCPlugin plugin) {
 		LWCProtocoLib.plugin = plugin;
 	}
+
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		LWC lwc = plugin.getLWC();
 		Player player = event.getPlayer();
 		Block block = event.getClickedBlock();
 		try {
-			if (lwc.findProtection(block) != null && (!lwc.canAccessProtection(player, block) || (!lwc.isAdmin(player) || !lwc.isMod(player)))) {
+			if (lwc.findProtection(block) != null) {
 				ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(lwc.getPlugin(),
 						ListenerPriority.MONITOR, PacketType.Play.Server.OPEN_WINDOW) {
 					@Override
 					public void onPacketSending(PacketEvent e) {
-						e.setReadOnly(false);
-						e.setCancelled(true);
-						lwc.sendLocale(player, "protection.interact.error.notowner", "block", LWC.materialToString(event.getClickedBlock()));
-						return;
+						if (lwc.isAdmin(player) || lwc.isMod(player)) {
+							e.setReadOnly(false);
+							e.setCancelled(false);
+							return;
+						} else if (lwc.canAccessProtection(player, block)) {
+							e.setReadOnly(false);
+							e.setCancelled(false);
+							return;
+						} else {
+							e.setReadOnly(false);
+							e.setCancelled(true);
+							return;	
+						}
 					}
 				});
 			}
