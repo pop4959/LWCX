213c4af44725b21ab0f41aec38f69a814241c434
Tanner <tsusergaming@gmail.com>
2018-08-17 00:45:23 -0700

    Fix attempt #1
    Made an error when merging, oops! Fixing things up.

--- src/main/java/com/griefcraft/bukkit/EntityBlockState.java
+++ src/main/java/com/griefcraft/bukkit/EntityBlockState.java
@@ -80,7 +80,6 @@ public class EntityBlockState implements BlockState{
     }
 
     @Override
-    @Deprecated
     public void setData(MaterialData materialData) {
         entityBlock.setData(materialData.getData());
         // TODO: What to do with it, deprecated?
