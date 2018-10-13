063c884388da1d3f7627e5204cf82fc0d7246984
pop4959 <pop4959@gmail.com>
2018-10-12 23:21:29 -0700

    fix pom + reformat code

---
+++ src/main/java/com/griefcraft/bukkit/EntityBlock.java
@@ -0,0 +1,306 @@
+package com.griefcraft.bukkit;
+
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.PistonMoveReaction;
+import org.bukkit.entity.Entity;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.MetadataValue;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Collection;
+import java.util.List;
+
+public class EntityBlock implements Block {
+    public static final int ENTITY_BLOCK_ID = 5000;
+    public static final int POSITION_OFFSET = 50000;
+    private Entity entity;
+
+    public EntityBlock(Entity entity) {
+        this.entity = entity;
+    }
+
+    @Override
+    public int getX() {
+        return 50000 + this.entity.getUniqueId().hashCode();
+    }
+
+    @Override
+    public int getY() {
+        return 50000 + this.entity.getUniqueId().hashCode();
+    }
+
+    @Override
+    public int getZ() {
+        return 50000 + this.entity.getUniqueId().hashCode();
+    }
+
+    @Override
+    public int getTypeId() {
+        return ENTITY_BLOCK_ID;
+    }
+
+    @Override
+    public World getWorld() {
+        return this.entity.getWorld();
+    }
+
+    public Entity getEntity() {
+        return this.entity;
+    }
+
+    public static Block getEntityBlock(Entity entity) {
+        return new EntityBlock(entity);
+    }
+
+    @Override
+    public List<MetadataValue> getMetadata(String arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean hasMetadata(String arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void removeMetadata(String arg0, Plugin arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setMetadata(String arg0, MetadataValue arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean breakNaturally() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean breakNaturally(ItemStack arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Biome getBiome() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getBlockPower() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getBlockPower(BlockFace arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Chunk getChunk() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public byte getData() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Collection<ItemStack> getDrops() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Collection<ItemStack> getDrops(ItemStack arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BlockFace getFace(Block arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double getHumidity() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public byte getLightFromBlocks() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public byte getLightFromSky() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public byte getLightLevel() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Location getLocation() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Location getLocation(Location arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public PistonMoveReaction getPistonMoveReaction() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Block getRelative(BlockFace arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Block getRelative(BlockFace arg0, int arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Block getRelative(int arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public BlockState getState() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public double getTemperature() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Material getType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isBlockFaceIndirectlyPowered(BlockFace arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isBlockFacePowered(BlockFace arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isBlockIndirectlyPowered() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isBlockPowered() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isLiquid() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void setBiome(Biome arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setData(byte arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setData(byte arg0, boolean arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void setType(Material arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setType(Material arg0, boolean arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean setTypeId(int arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean setTypeId(int arg0, boolean arg1) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean setTypeIdAndData(int arg0, byte arg1, boolean arg2) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
---
+++ src/main/java/com/griefcraft/util/matchers/GravityMatcher.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2011 Tyler Blair. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and contributors and should not be interpreted as representing official policies,
+ * either expressed or implied, of anybody else.
+ */
+
+package com.griefcraft.util.matchers;
+
+import com.griefcraft.util.ProtectionFinder;
+import com.griefcraft.util.SetUtil;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+/**
+ * Matches blocks such as the sign post
+ */
+@SuppressWarnings("deprecation")
+public class GravityMatcher implements ProtectionFinder.Matcher {
+
+    public static final Set<Material> PROTECTABLES_POSTS = EnumSet.of(
+            Material.SIGN_POST, Material.RAILS, Material.POWERED_RAIL,
+            Material.DETECTOR_RAIL, Material.LEVER, Material.STONE_BUTTON,
+            Material.WOOD_BUTTON, Material.STONE_PLATE, Material.WOOD_PLATE,
+            Material.IRON_PLATE, Material.GOLD_PLATE);
+
+    static {
+        SetUtil.addToSetWithoutNull(PROTECTABLES_POSTS,
+                Material.getMaterial(416));
+        SetUtil.addToSetWithoutNull(PROTECTABLES_POSTS,
+                Material.getMaterial(176)); // Standing banner
+    }
+
+    public boolean matches(ProtectionFinder finder) {
+        Block block = finder.getBaseBlock().getBlock();
+
+        // Easy to match, just try to match the block above the base block :P
+        Block up = block.getRelative(BlockFace.UP);
+
+        if (PROTECTABLES_POSTS.contains(up.getType())) {
+            finder.addBlock(up);
+            return true;
+        }
+        return false;
+    }
+
+}
