294ad2fc2e6855d922b61e48313c6751092c6cc5
Zero <zero@cock.li>
2019-08-19 17:40:57 -0400

    Support new Factions code

--- src/main/java/com/griefcraft/modules/pluginsupport/Factions.java
+++ src/main/java/com/griefcraft/modules/pluginsupport/Factions.java
@@ -3,14 +3,14 @@ package com.griefcraft.modules.pluginsupport;
 import com.griefcraft.lwc.LWC;
 import com.griefcraft.scripting.JavaModule;
 import com.griefcraft.scripting.event.LWCProtectionRegisterEvent;
-import com.massivecraft.factions.Conf;
 import com.massivecraft.factions.Board;
 import com.massivecraft.factions.FLocation;
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
 import com.massivecraft.factions.Faction;
-import com.massivecraft.factions.zcore.fperms.Access;
-import com.massivecraft.factions.zcore.fperms.PermissableAction;
+import com.massivecraft.factions.FactionsPlugin;
+import com.massivecraft.factions.config.file.MainConfig.Factions.Protection;
+import com.massivecraft.factions.perms.PermissibleAction;
 import org.bukkit.plugin.Plugin;
 
 public class Factions extends JavaModule {
@@ -31,26 +31,18 @@ public class Factions extends JavaModule {
             return;
         }
 
-        boolean canProtect = false;
         FLocation fLocation = new FLocation(event.getBlock().getLocation());
         FPlayer fPlayer = FPlayers.getInstance().getByPlayer(event.getPlayer());
         Faction faction = Board.getInstance().getFactionAt(fLocation);
-        Faction myFaction = fPlayer.getFaction();
-        boolean fBypass = Conf.playersWhoBypassAllProtection.contains(fPlayer.getName()) || fPlayer.isAdminBypassing();
-        boolean fWilderness = faction.isWilderness() && !Conf.wildernessDenyBuild;
-        boolean fWarzone = faction.isWarZone() && !Conf.warZoneDenyBuild;
-        boolean fSafezone = faction.isSafeZone() && !Conf.safeZoneDenyBuild;
+        Protection prot = FactionsPlugin.getInstance().conf().factions().protection();
 
-        if (fBypass || fWilderness || fWarzone || fSafezone) {
-            canProtect = true;
-        } else if (!myFaction.getRelationTo(faction).confDenyBuild(faction.hasPlayersOnline())) {
-            canProtect = true;
-        } else {
-            Access fAccess = faction.getAccess(fPlayer, PermissableAction.BUILD);
-            if (fAccess != null) { canProtect = (fAccess == Access.ALLOW); }
-        }
+        boolean fBypass = prot.getPlayersWhoBypassAllProtection().contains(fPlayer.getName())
+                || fPlayer.isAdminBypassing();
+        boolean fWilderness = faction.isWilderness() && !prot.isWildernessDenyBuild();
+        boolean fWarzone = faction.isWarZone() && !prot.isWarZoneDenyBuild();
+        boolean fSafezone = faction.isSafeZone() && !prot.isSafeZoneDenyBuild();
 
-        if (!canProtect) {
+        if (!(fBypass || fWilderness || fWarzone || fSafezone) && !faction.hasAccess(fPlayer, PermissibleAction.BUILD)) {
             event.getLWC().sendLocale(event.getPlayer(), "lwc.factions.blocked");
             event.setCancelled(true);
         }
