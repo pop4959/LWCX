7fd64e43fc41002d32b4096e475c5b1301677c48
pop4959 <pop4959@gmail.com>
2022-10-10 18:22:50 -0700

    Build script changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,44 +1,24 @@
-import org.ajoberstar.grgit.Grgit
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
-    java
-    jacoco
-    `maven-publish`
-    id("org.ajoberstar.grgit") version "4.1.1"
+    id("java-library")
+    id("maven-publish")
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
-logger.lifecycle("""
-*******************************************
- You are building LWCX!
- If you encounter trouble:
- 1) Try running 'build' in a separate Gradle run
- 2) Use gradlew and not gradle
- 3) If you still need help, ask on Discord! https://discord.gg/PHpuzZS
-
- Output files will be in build/libs
-*******************************************
-""")
-
-if (!project.hasProperty("gitCommitHash")) {
-    apply(plugin = "org.ajoberstar.grgit")
-    ext["gitCommitHash"] = try {
-        Grgit.open(mapOf("currentDir" to project.rootDir))?.head()?.abbreviatedId
-    } catch (e: Exception) {
-        logger.warn("Error getting commit hash", e)
-
-        "no.git.id"
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
     }
+    withSourcesJar()
 }
 
 repositories {
     mavenCentral()
-
-    maven { url = uri("https://repo.papermc.io/repository/maven-public/") }
-    maven { url = uri("https://maven.enginehub.org/repo/") }
-    maven { url = uri("https://repo.glaremasters.me/repository/public/") }
-    maven { url = uri("https://jitpack.io") }
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://maven.enginehub.org/repo/")
+    maven("https://repo.glaremasters.me/repository/towny/")
+    maven("https://ci.ender.zone/plugin/repository/everything/")
+    maven("https://repo.glaremasters.me/repository/public/")
+    maven("https://jitpack.io")
 }
 
 configurations {
@@ -46,59 +26,41 @@ configurations {
 }
 
 dependencies {
-    compileOnly("org.jetbrains:annotations:23.0.0")
-
-    compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
-    compileOnly("com.googlecode.json-simple:json-simple:1.1.1")
-    compileOnly("commons-lang:commons-lang:2.6")
-
-    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.12")
-    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.7")
-    compileOnly("com.github.TownyAdvanced:Towny:0.98.3.7")
-    compileOnly("com.massivecraft:Factions:1.6.9.5-U0.4.9") { isTransitive = false }
-    compileOnly("com.github.milkbowl:VaultAPI:1.7.1")
-    "shadeOnly"("org.bstats:bstats-bukkit:3.0.0")
-
-}
-
-tasks.named<Copy>("processResources") {
-    val internalVersion = "$version-${rootProject.ext["gitCommitHash"]}"
-    inputs.property("internalVersion", internalVersion)
-
-    filesMatching("plugin.yml") {
-        expand("internalVersion" to internalVersion)
+    compileOnly(group = "org.spigotmc", name = "spigot-api", version = "1.19.2-R0.1-SNAPSHOT")
+    compileOnly(group = "com.sk89q.worldedit", name = "worldedit-core", version = "7.1.0")
+    compileOnly(group = "com.sk89q.worldguard", name = "worldguard-bukkit", version = "7.0.0")
+    compileOnly(group = "com.palmergames.bukkit.towny", name = "towny", version = "0.98.2.0")
+    compileOnly(group = "com.massivecraft", name = "Factions", version = "1.6.9.5-U0.4.9") {
+        isTransitive = false
     }
+    compileOnly(group = "com.github.MilkBowl", name = "VaultAPI", version = "1.7.1")
+    compileOnly(group = "com.google.guava", name = "guava", version = "23.0")
+    implementation(group = "org.bstats", name = "bstats-bukkit", version = "3.0.0")
 }
 
-tasks.named<Jar>("jar") {
-    val projectVersion = project.version
-    inputs.property("projectVersion", projectVersion)
-    manifest {
-        attributes("Implementation-Version" to projectVersion)
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
     }
-}
-
-tasks.named<ShadowJar>("shadowJar") {
-    configurations = listOf(project.configurations["shadeOnly"], project.configurations["runtimeClasspath"])
-
-    dependencies {
-        relocate("org.bstats", "com.griefcraft.shaded.bstats") {
-            include(dependency("org.bstats:"))
-        }
-        relocate("com.mysql", "com.griefcraft.shaded.mysql") {
-            include(dependency("mysql:"))
+    jar {
+        archiveClassifier.set("noshade")
+    }
+    processResources {
+        filesMatching("plugin.yml") {
+            expand(
+                "version" to project.version,
+            )
         }
     }
+    shadowJar {
+        archiveClassifier.set("")
+        archiveFileName.set("${rootProject.name.toUpperCase()}-${project.version}.jar")
+        relocate("org.bstats", "${project.group}.${rootProject.name}.lib.bstats")
+    }
+    build {
+        dependsOn(shadowJar)
+    }
 }
-
-tasks.named("assemble").configure {
-    dependsOn("shadowJar")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
-
 
 publishing {
     repositories {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,1 @@
-rootProject.name = "LWCX"
-
-pluginManagement {
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
+rootProject.name = "lwc"
